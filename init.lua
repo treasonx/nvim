5778febb6101249ebd6cdeab2291157c4f30e103
James Morrin <james@morr.in>
2025-07-09 08:42:57 -0700

    more plugins for markdown

--- init.lua
+++ init.lua
@@ -33,3 +33,50 @@ vim.api.nvim_create_autocmd("VimEnter", {
 vim.opt.termguicolors = true
 
 require("config.lazy")
+
+-- spell check for markdown
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+  end,
+})
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+local function spell_suggestions()
+  local word = vim.fn.expand('<cword>') -- Get word under cursor
+  local suggestions = vim.fn.spellsuggest(word, 20) -- Get up to 20 suggestions
+  if #suggestions == 0 then
+    vim.notify("No spelling suggestions for '" .. word .. "'", vim.log.levels.INFO)
+    return
+  end
+
+  pickers.new({}, {
+    prompt_title = "Spelling Suggestions for '" .. word .. "'",
+    finder = finders.new_table({
+      results = suggestions,
+    }),
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        vim.cmd('normal! ciw' .. selection[1]) -- Replace word with suggestion
+      end)
+      return true
+    end,
+    layout_strategy = 'cursor', -- Position near cursor
+    layout_config = {
+      width = 0.4, -- 40% of window width
+      height = 0.5, -- 50% of window height
+    },
+  }):find()
+end
+
+-- Replace your existing <leader>s keybinding
+vim.keymap.set("n", "<leader>s", spell_suggestions, { desc = "Spell suggestions dropdown" })
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,174 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" }, -- Load on buffer events
+  dependencies = {
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "L3MON4D3/LuaSnip" },
+        { "saadparwaiz1/cmp_luasnip" },
+      },
+    },
+  },
+  opts = function()
+    -- Mason setup
+    local mason = {
+      ui = {
+        border = "rounded",
+      },
+    }
+
+    -- Mason-lspconfig setup
+    local mason_lspconfig = {
+      --ensure_installed = { "lua_ls", "pyright", "ts_ls", "markdown" }, -- Added tsserver
+      --automatic_installation = true,
+    }
+
+    -- LSP server configurations
+    local lspconfig = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }, -- Recognize Neovim globals
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true), -- Neovim API
+              },
+            },
+          },
+        },
+        pyright = {}, -- Minimal config for Python
+        ts_ls = {}, -- Minimal config for JavaScript/TypeScript
+      },
+      -- Global LSP setup
+      setup = function()
+        vim.diagnostic.config({
+          virtual_text = true, -- Inline diagnostics
+          signs = true, -- Gutter signs
+          update_in_insert = false,
+        })
+      end,
+    }
+
+    -- nvim-cmp setup
+    local cmp = {
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = require("cmp").mapping.preset.insert({
+        ["<C-b>"] = require("cmp").mapping.scroll_docs(-4),
+        ["<C-f>"] = require("cmp").mapping.scroll_docs(4),
+        ["<C-Space>"] = require("cmp").mapping.complete(),
+        ["<C-e>"] = require("cmp").mapping.abort(),
+        ["<CR>"] = require("cmp").mapping.confirm({ select = true }),
+        ["<Tab>"] = require("cmp").mapping(function(fallback)
+          if require("cmp").visible() then
+            require("cmp").select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            require("luasnip").expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = require("cmp").mapping(function(fallback)
+          if require("cmp").visible() then
+            require("cmp").select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            require("luasnip").jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+    }
+
+    return {
+      mason = mason,
+      mason_lspconfig = mason_lspconfig,
+      lspconfig = lspconfig,
+      cmp = cmp,
+    }
+  end,
+  config = function(_, opts)
+    -- Setup Mason
+    require("mason").setup(opts.mason)
+
+    -- Setup Mason-lspconfig
+    require("mason-lspconfig").setup(opts.mason_lspconfig)
+
+    -- Setup nvim-cmp
+    require("cmp").setup(opts.cmp)
+
+    -- Setup LSP servers
+    local lspconfig = require("lspconfig")
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+    for server, config in pairs(opts.lspconfig.servers) do
+      config.capabilities = capabilities
+      lspconfig[server].setup(config)
+    end
+
+    -- Apply global LSP setup
+    opts.lspconfig.setup()
+  end,
+  keys = {
+    {
+      "<leader>gd",
+      function()
+        vim.lsp.buf.definition()
+      end,
+      desc = "Go to definition",
+    },
+    {
+      "<leader>gr",
+      function()
+        vim.lsp.buf.references()
+      end,
+      desc = "Find references",
+    },
+    {
+      "<leader>ca",
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      desc = "Code action",
+    },
+    {
+      "<leader>rn",
+      function()
+        vim.lsp.buf.rename()
+      end,
+      desc = "Rename symbol",
+    },
+    {
+      "<leader>ds",
+      function()
+        vim.diagnostic.open_float()
+      end,
+      desc = "Show diagnostics",
+    },
+    {
+      "K",
+      function()
+        vim.lsp.buf.hover()
+      end,
+      desc = "Hover documentation",
+    },
+  },
+}
